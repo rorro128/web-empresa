d4401c3ef3bf673c17c4191a327a1090f8be1c82
rorro128 <rodrigo.tardone@gmail.com>
2018-12-26 21:16:16 -0300

    Initial commit

---
+++ js/purchase.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    var $buythemediv = '<div class="buy-theme alt-font xs-display-none"><a href="https://themeforest.net/item/leadgen-multipurpose-marketing-landing-page-pack-with-page-builder/19245368?ref=themezaa" target="_blank"><span>Purchase Theme</span></a></div><div class="quick-question alt-font xs-display-none"><a href="../index.html"><span>All Templates</span></a></div>';
+    $('body').append($buythemediv);
+});
